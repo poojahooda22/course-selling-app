c882e999c1bfff558e1a4b8cd6e3ebcfcd2dca8d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-05 22:06:26 +0530

    updated text

--- src/Signin.jsx
+++ src/Signin.jsx
@@ -49,7 +49,8 @@ function Signin() {
                         flexDirection: 'column',          
                     }}
                 >
-                    <TextField id="outlined-basic" label="Email" variant="outlined" size="small" margin="normal" />
+                    <TextField 
+                    id="outlined-basic" label="Email" variant="outlined" size="small" margin="normal" />
                     <TextField id="outlined-basic" label="Password" type='password' variant="outlined" size="small" margin="normal"/>
                 </div>
                 <Button 
