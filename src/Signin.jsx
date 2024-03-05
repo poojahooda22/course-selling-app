2ea516985ed1cf9cb0fc1c9a0189deb27dcaba4b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-05 22:02:11 +0530

    updated

--- src/Signin.jsx
+++ src/Signin.jsx
@@ -29,7 +29,8 @@ function Signin() {
         <div 
             style={{display: "flex",
              justifyContent: "center"
-             }}>
+             }}
+             >
             <Card 
                 style={{padding: '16px', width: 400, display:"flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
                 <div 
