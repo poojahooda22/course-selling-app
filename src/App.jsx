3434fc0bb80039ee0da001bc0c036bb7e50d0a8f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-05 20:20:14 +0530

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/addcourse" element={<AddCourse />} />
-          <Route ></Route>
+          <Route path=""></Route>
           <Route path="/login" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
