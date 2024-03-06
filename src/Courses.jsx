2729707fdc7cbd5dd3ff730a5e85e0a6ae59d60c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-06 11:54:15 +0530

    updated courses

--- src/Courses.jsx
+++ src/Courses.jsx
@@ -3,6 +3,14 @@ import { useEffect, useState } from "react";
 function Courses() {
   const [courses, setCourses] = useState([]);
 
+  useEffect(() => {
+    fetch("http://localhost:3000/admin/courses", {
+      method: "GET",
+      headers: {
+        "Authorization": "Bearer " + localStorage.getItem("token"),
+    }
+    })
+  })
   
   return (
     <div>
