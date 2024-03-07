c9be1e5273c03b1009767fde4fa85f5f4b5fec7b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-07 16:54:20 +0530

    updated courses

--- src/CourseUpdate.jsx
+++ src/CourseUpdate.jsx
@@ -1,13 +1,37 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
+import {useParams} from 'react-router-dom'
+import Course from './Course';
 
 function CourseUpdate() {
+    let { courseId} = useParams();
+    const[courses, setCourses] = useState([]);
 
     useEffect(() => {
+        function callback2(data) {
+          setCourses(data.courses);
+        }
+        function callback1(res) {
+          res.json().then(callback2);
+        }
+    
+        fetch("http://localhost:3000/admin/courses/", {
+          method: "GET",
+          headers: {
+            "Authorization": "Bearer " + localStorage.getItem("token"),
+          }
+        }).then(callback1)
+    }, []);
 
-    })
+    let course = null;
+    for(let i = 0; i< courses.length; i++) {
+        if(courses[i].id == courseId) [
+            course = courses[i]
+        ]
+    }
+
     return (
         <div>
-            <h1>Course Update</h1>
+            <Course course={course} />
         </div>
     )
 }
