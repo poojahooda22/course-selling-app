c2f3b21d0373b240728824f5f6064550ba7613b9
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-07 16:13:40 +0530

    updated courses

--- src/Courses.jsx
+++ src/Courses.jsx
@@ -21,8 +21,11 @@ function Courses() {
     }).then(callback1)
   }, []);
 
-  return 
-      
+  return <div style={{display: 'flex', flexWrap: 'wrap'}}>
+    {courses.map(course => (
+      <Course course={course} key={course.id} />
+    ))} 
+  </div>
    
 }  
 
