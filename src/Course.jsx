7412565557803ccaa990b8056af27d829c691c3e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-07 17:47:59 +0530

    updated card

--- src/Course.jsx
+++ src/Course.jsx
@@ -6,7 +6,7 @@ function Course(props) {
     return <div style={{ margin: '16px'}}>
         <Card style={{
             padding: '16px', 
-            width: 400, 
+            width: 300, 
             display:"flex", 
             flexDirection: "column", 
             justifyContent: "center", 
@@ -23,12 +23,12 @@ function Course(props) {
             >
                 {props.course.title}
             </Typography>
-            <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between',  width: '400px', marginTop: '12px' }}>
+            <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between',  width: '300px', marginTop: '12px' }}>
                 
                 <Typography variant="h6" component="div">Rs {props.course.price}</Typography>
                 <Typography variant="h6" component="div" style={{color: '#28c271'}}>35% off</Typography>
             </div>
-            <Typography variant="body1" component="div" style={{textAlign: 'left', width: '400px', margin: '12px'}}>{props.course.description}</Typography>
+            <Typography variant="body1" component="div" style={{textAlign: 'left', width: '300px', margin: '12px'}}>{props.course.description}</Typography>
         </Card>
     </div>
  }
