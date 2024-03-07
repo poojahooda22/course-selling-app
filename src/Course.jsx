7c3f002e7aaf77b67eddf67648d161fb612d9369
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-07 15:29:12 +0530

    updated courses

--- src/Course.jsx
+++ src/Course.jsx
@@ -1,17 +1,24 @@
 import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
+import CardMedia from '@mui/material/CardMedia';
 
 function Course(props) {
-    return <Card style={{
-        padding: '16px', 
-        width: 400, 
-        display:"flex", 
-        flexDirection: "column", 
-        justifyContent: "center", 
-        alignItems: "center" 
-    }}>
-     {props.course.title}
-    </Card>
+    return <div style={{ margin: '16px'}}>
+        <Card style={{
+            padding: '16px', 
+            width: 400, 
+            display:"flex", 
+            flexDirection: "column", 
+            justifyContent: "center", 
+            alignItems: "center" 
+        }}>
+            <img
+                src={props.course.imageLink}
+                style={{width: '100%', height: '200px', objectFit: 'cover', borderRadius: '8px'}}
+            />
+            <Typography variant="h5" component="div">{props.course.title}</Typography>
+        </Card>
+    </div>
  }
 
 
