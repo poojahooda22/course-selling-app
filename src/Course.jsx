90a23ffd7ec70f7ffe74004131e19ff56175ef0f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-07 21:55:29 +0530

    updated courses

--- src/Course.jsx
+++ src/Course.jsx
@@ -27,7 +27,8 @@ function Course(props) {
             </Typography>
             <div 
                 style={{
-                    display: 'flex', alignItems: 'center', justifyContent: 'space-between',  width: '300px', marginTop: '12px' }}>
+                    display: 'flex', alignItems: 'center', justifyContent: 'space-between',  width: '300px', marginTop: '12px' 
+                    }}>
                 
                 <Typography variant="h6" component="div">Rs {props.course.price}</Typography>
                 <Typography variant="h6" component="div" style={{color: '#28c271'}}>35% off</Typography>
