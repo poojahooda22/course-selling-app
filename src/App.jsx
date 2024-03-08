050fc9cc01934a1cded0302e0e75adc706c1a678
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-08 22:42:44 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import Courses from './Courses';
 import CourseUpdate from './CourseUpdate';
 
 function App() {
-  
   return (
     <div style={{width: "100vw", height:"100vh", backgroundColor: "#eeeeee" }}>
       <Appbar />
