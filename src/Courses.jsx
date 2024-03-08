68c70b98925e665c72ac20668b622a19ef086ec0
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-08 22:47:07 +0530

    update

--- src/Courses.jsx
+++ src/Courses.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Course from "./Course";
-import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version
 
 function Courses() {
   const [courses, setCourses] = useState([]);
