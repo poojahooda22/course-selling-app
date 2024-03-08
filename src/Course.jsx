80af7c8611900718e71ff9b48505c8dc861c046d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-08 22:11:43 +0530

    updated

--- src/Course.jsx
+++ src/Course.jsx
@@ -52,7 +52,8 @@ function Course(props) {
                 component="div" 
                 style={{
                     textAlign: 'left', 
-                    width: '300px', margin: '12px'
+                    width: '300px', 
+                    margin: '12px'
                 }}
             >
                 {props.course.description}
