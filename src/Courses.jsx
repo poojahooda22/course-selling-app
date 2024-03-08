7253ad56e92797222b4f20626ab7beb66777ac07
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-08 22:52:43 +0530

    courses

--- src/Courses.jsx
+++ src/Courses.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Course from "./Course";
-import Grid from '@mui/material/U
+import Grid from '@mui/material/
 
 function Courses() {
   const [courses, setCourses] = useState([]);
