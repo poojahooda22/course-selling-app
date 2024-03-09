e14fc208b48f1a6332230cb4d9f52aded255e201
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-09 22:05:23 +0530

    updated router

--- server/Routes/admin.js
+++ server/Routes/admin.js
@@ -2,3 +2,6 @@ const mongoose = require("mongoose");
 const express = require('express');
 const { User, Course, Admin } = require("./db");
 const jwt = require('jsonwebtoken');
+
+const router = express.Router;
+
