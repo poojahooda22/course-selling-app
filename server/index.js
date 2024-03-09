ecff538be2f40e0507211ce5c2d6fa4c156cb93d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-09 21:43:46 +0530

    updated app

--- server/index.js
+++ server/index.js
@@ -14,3 +14,5 @@ app.use("/user", userRouter);
 
 //connect to MongoDB
 mongoose.connect("mongodb+srv://phooda938:phooda938@cluster0.kof5uvs.mongodb.net/", {useNewUrlParser: true, useUnifiedTopology: true} )
+
+app.listen
