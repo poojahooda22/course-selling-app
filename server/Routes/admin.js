138d5365307983203912ed81a31db62167727fa3
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-09 22:05:33 +0530

    updated admin

--- server/Routes/admin.js
+++ server/Routes/admin.js
@@ -5,3 +5,15 @@ const jwt = require('jsonwebtoken');
 
 const router = express.Router;
 
+app.post('/admin/signup', async (req, res) => {
+    const {username, password} = req.body;
+    const admin = await Admin.findOne({ username});
+    if(admin) {
+        res.status(403).json({message: 'Admin already exists'});
+    } else {
+        const newAdmin = new Admin({ username, password});
+        await newAdmin.save();
+        const token = jwt.sign({username, role: 'admin'}, SECRET, {expiresIn: '1h'});
+        res.json({message: 'Admin created successfully', token});
+    }
+});
