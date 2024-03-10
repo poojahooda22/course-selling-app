dd268692f532cb5b21923ca3fac566328a734f6c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-10 19:17:45 +0530

    updated user

--- server/Routes/user.js
+++ server/Routes/user.js
@@ -1,3 +1,5 @@
 const express = require('express');
 const {authenticateJwt, SECRET} = require('../middleware/auth');
 const {User, Course, Admin} = require("../db");
+
+const router = express.Router();
