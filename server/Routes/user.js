41005075547289476e7aefce02a7444c2cce8886
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-10 19:22:05 +0530

    updated user

--- server/Routes/user.js
+++ server/Routes/user.js
@@ -3,3 +3,16 @@ const {authenticateJwt, SECRET} = require('../middleware/auth');
 const {User, Course, Admin} = require("../db");
 
 const router = express.Router();
+
+app.post('/users/signup', async (req, res) => {
+    const {username, password} = req.body;
+    const user = await User.findOne({ username});
+    if(user) {
+        res.status(403).json({message: 'User already exists'});
+    } else {
+        const newUser = new User({ username, password});
+        await newUser.save();
+        const token = jwt.sign({username, role: 'user'}, SECRET, {expiresIn: '1h'});
+        res.json({message: 'User created successfully', token});
+    }
+});
