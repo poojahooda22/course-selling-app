57f69618a007ac50819db31b49aa25329322550d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-10 18:28:39 +0530

    updated user schema

--- server/db/index.js
+++ server/db/index.js
@@ -2,3 +2,9 @@ const mongoose = require("mongoose");
 
 
 //define mongoose schemas
+
+const userSchema = new mongoose.Schema({
+    username: {type: String},
+    password: String,
+    purchasedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course'}]
+});
