0b987cc38ddf637d93ae6c7e47546c22ab39d257
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-12 21:26:06 +0530

    updated course

--- src/Course.jsx
+++ src/Course.jsx
@@ -45,7 +45,7 @@ function Course(props) {
                 </Typography>
                 {/* <T 
                     variant="h6" 
-                    c     
+                
             } */}
             </div>
             <Typography 
