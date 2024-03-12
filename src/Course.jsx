9409ce68991779518b746606a063ab54ec02780b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-12 21:19:29 +0530

    updated course

--- src/Course.jsx
+++ src/Course.jsx
@@ -49,7 +49,7 @@ function Course(props) {
                     style={{color: '#28c271'}}
                 >
                     35% off
-                 }
+                 
             </div>
             <Typography 
                 variant="body1" 
