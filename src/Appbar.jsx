7df3a857577a8c23494b0d30149d5a0c9745fd28
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-19 21:17:52 +0530

    add

--- src/Appbar.jsx
+++ src/Appbar.jsx
@@ -75,7 +75,7 @@ function Appbar() {
                     {/* <*>
                         <Avatar 
                             src="/broken-image.jpg" 
-                            s
+                            
                     */}
 
                     <Button 
