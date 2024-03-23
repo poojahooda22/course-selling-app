3fa82e928d8c7882f8d16e149aecd8c1a09a1919
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-23 13:35:29 +0530

    updated landing page

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,7 +1,45 @@
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
 function LandingPage() {
     return (
         <div>
-            <h2>Welcome to the course</h2>
+            <div>
+                <Typography 
+                    variant="h1" 
+                    component="div"
+                >
+                    Course Admin
+                </Typography>
+                <Typography 
+                    variant="h6" 
+                    component="div"
+                    style={{marginBottom: "16px"}}
+                >
+                    A place to learn, earn and grow
+                </Typography>
+                <Button 
+                    variant="contained" 
+                    size="small"
+                    style={{marginRight: "16px"}}
+                    color={"primary"}  
+                    onClick={() => {
+                        window.location = "/signup"
+                    }}  
+                >
+                    Signup
+                </Button>
+                <Button 
+                    variant="contained" 
+                    size="small"
+                    color={"secondary"}  
+                    onClick={() => {
+                        window.location = "/login"
+                    }}     
+                >
+                    Login
+                </Button>
+            </div>   
         </div>
     )
 }
