3d74eb8c9ec81c9936fd0cf50d1526953483a52a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-09 21:12:36 +0530

    updated appbar

--- src/Appbar.jsx
+++ src/Appbar.jsx
@@ -1,7 +1,29 @@
 import Button from '@mui/material/Button';
+import { useEffect } from 'react';
+
 
 const Appbar = () => {
-  return (
+
+    useEffect(() => {
+
+        function callback2(data) {
+            console.log(data)
+        }
+
+        function callbackFn(res) {
+            res.json().then(callback2)
+        }
+
+        fetch("http://localhost:3000/admin/me", {
+            method: "GET",
+            headers: {
+              "Authorization": "Bearer" + localStorage.getItem("token")
+            }
+        }).then(callbackFn)
+    })
+
+
+    return (
     <>
         <div className='w-full flex items-center justify-between px-[2vw]'>
             <h2 className='text-[1.5vw] font-bold'>Courera</h2>
