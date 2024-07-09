330080cef8f7d65269f4f54a0209dda0a079332e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-09 20:46:33 +0530

    removed usestate

--- src/AddCourse.jsx
+++ src/AddCourse.jsx
@@ -2,7 +2,7 @@
 import Card from '@mui/material/Card';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import { useState } from 'react';
+
 // import axios from 'axios';
 
 
