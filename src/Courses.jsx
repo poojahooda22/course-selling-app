a7592df6692f537281a35227d62f18936e13e76f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-10 14:27:31 +0530

    updated courses

--- src/Courses.jsx
+++ src/Courses.jsx
@@ -1,32 +1,11 @@
-import { useEffect, useState } from "react";
-import Course from "./Course";
 
 
-function Courses() {
-  const [courses, setCourses] = useState([]);
+const Courses = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
 
-  useEffect(() => {
-    function callback2(data) {
-      setCourses(data.courses);
-    }
-    function callback1(res) {
-      res.json().then(callback2);
-    }
-
-    fetch("http://localhost:3000/admin/courses", {
-      method: "GET",
-      headers: {
-        "Authorization": "Bearer " + localStorage.getItem("token"),
-      }
-    }).then(callback1)
-  }, []);
-
-  return <div style={{display: 'flex', flexWrap: 'wrap', justifyCenter: 'center'}}>
-    {courses.map(course => (
-      <Course course={course} key={course.id} />
-    ))} 
-  </div>
-   
-}  
-
-export default Courses;
+export default Courses
