edd9376e434f7ff07a98dfaa7bbb557eeeda0b26
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-10 14:32:25 +0530

    added cards

--- src/Courses.jsx
+++ src/Courses.jsx
@@ -2,12 +2,29 @@ import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
+import Typography from '@mui/material/Typography';
 
 const Courses = () => {
   return (
-    <card sx={{ maxWidth: 345 }}>
-      
-    </card>
+    <div>
+        <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+        component="img"
+        height="194"
+        image="/static/images/cards/paella.jpg"
+        alt="Paella dish"
+        />
+      <CardContent>
+        <Typography variant="body1" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+        </Typography>
+        <Typography paragraph>
+            Heat 1/2 cup of the 
+          </Typography>
+      </CardContent>
+    </Card>
+    </div>
+    
   )
 }
 
