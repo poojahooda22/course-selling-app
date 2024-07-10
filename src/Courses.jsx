acb22c115f9b4149a041b0f3112db84f785329f4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-10 14:43:56 +0530

    added courses

--- src/Courses.jsx
+++ src/Courses.jsx
@@ -3,25 +3,29 @@ import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 const Courses = () => {
   return (
-    <div className='flex items-center'>
-      <Card sx={{ maxWidth: 345 }}>
+    <div className='flex items-center justify-center'>
+      <Card sx={{ minWidth: 345 }}>
         <CardMedia
-          component="img"
-          height="194"
-          image="/static/images/cards/paella.jpg"
+           sx={{ height: 140 }}
+          image="https://img.freepik.com/free-vector/e-learning-interactions-illustration-concept_114360-23713.jpg?ga=GA1.1.678771452.1700385267&semt=sph"
           alt="Paella dish"
         />
         <CardContent>
-          <Typography variant="body1" color="text.secondary">
-            This impressive paella is a perfect party dish and a fun meal to cook
+          <Typography gutterBottom variant="h5" color="text.secondary">
+            Course1
           </Typography>
-          <Typography paragraph>
+          <Typography variant="body">
             Heat 1/2 cup of the 
           </Typography>
         </CardContent>
+        <CardActions>
+        <Button size="small">Edit</Button>
+        <Button size="small">Delete</Button>
+      </CardActions>
       </Card>
     </div>
     
