55461d6fbf7ded9ff5d5b9b56e780d682afc15a6
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-10 18:05:24 +0530

    updated routes

--- server/Routes/admin.js
+++ server/Routes/admin.js
@@ -37,7 +37,7 @@ router.post('/login', async(req, res) => {
   const {username, password} = req.headers;
   const admin = await Admin.findOne({username, password});
   if(admin) {
-      const token = jwt.sign({username, role: 'admin'}, SECRET, {expiresIn: '1h'});
+    const token = jwt.sign({username, role: 'admin'}, SECRET, {expiresIn: '1h'});
       res.json({message: 'Logged in successfully', token});
   } else {
       res.status(403).json({message: 'Invalid or password'});
