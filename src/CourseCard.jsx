c3f834c7f0c6f6ae9be1cb3cf197f502f59e2cab
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-11 16:42:44 +0530

    updated card

--- src/CourseCard.jsx
+++ src/CourseCard.jsx
@@ -14,7 +14,7 @@ const CourseCard = () => {
         function callback1(res) {
         res.json().then(callback2)
         }
-        fetch('http://localhost:3000/admin/courses/', {
+        fetch('http://localhost:3000/admin/courses', {
             method: "GET",
             headers: {
                 "Authorization": `Bearer ${localStorage.getItem("token")}`
