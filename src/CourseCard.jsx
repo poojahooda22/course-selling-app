6009a99d492d2b16c46153792325d728d765bae5
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-11 14:37:51 +0530

    updated course card

--- src/CourseCard.jsx
+++ src/CourseCard.jsx
@@ -1,11 +1,32 @@
+import Course from "./Course"
+import { useEffect, useState } from 'react';
 
 
 const CourseCard = () => {
-  return (
-    <div>
-      <h1>Course card</h1>
-    </div>
-  )
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        function callback2(data){
+        setCourses(data.courses);
+        }
+        function callback1(res) {
+        res.json().then(callback2)
+        }
+        fetch('http://localhost:3000/admin/courses', {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${localStorage.getItem("token")}`
+        }
+        }).then(callback1)
+    })
+
+    return (
+        <div>
+            <h1>Course card</h1>
+        
+        </div>
+    )
 }
 
 export default CourseCard
