f18220ba5be92daa20687c483b9d2a5d0659a8de
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-11 13:12:33 +0530

    updated courses

--- src/Courses.jsx
+++ src/Courses.jsx
@@ -29,25 +29,7 @@ const Courses = () => {
       {courses.map((course, index) => {
         return (
           <div key={index}>
-            <Card sx={{ minWidth: 345 }}>
-              <CardMedia
-                sx={{ height: 140 }}
-                image={course.imageLink}
-                alt="Paella dish"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" color="text.secondary">
-                  {course.title}
-                </Typography>
-                <Typography variant="body">
-                  {course.description}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Edit</Button>
-                <Button size="small">Delete</Button>
-              </CardActions>
-            </Card>
+            
           </div> 
         )
       })}
