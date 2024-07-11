2dc631b2494ac303260a3b89aeca64da54544361
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-11 16:07:19 +0530

    added

--- src/CourseCard.jsx
+++ src/CourseCard.jsx
@@ -9,7 +9,7 @@ const CourseCard = () => {
 
     useEffect(() => {
         function callback2(data){
-        setCourses(data.courses);
+            setCourses(data.courses);
         }
         function callback1(res) {
             res.json().then(callback2)
