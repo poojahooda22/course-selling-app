6a93c05e4de2000af44ce35abda116d5a6001bd4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-11 13:10:07 +0530

    added

--- src/Courses.jsx
+++ src/Courses.jsx
@@ -18,7 +18,7 @@ const Courses = () => {
     }
     fetch('http://localhost:3000/admin/courses', {
         method: "GET",
-        // body: JSON.stringify({
+        // body: 
         headers: {
             "Content-Type": "application/json",
             "Authorization": `Bearer ${localStorage.getItem("token")}`
