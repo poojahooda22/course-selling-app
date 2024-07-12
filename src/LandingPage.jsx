cba56b034f0a5779136af2fa70c56e2e8abcc16e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-12 20:27:00 +0530

    removed div

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -3,6 +3,7 @@ import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 
 function LandingPage() {
+    
     return (
         <div>
         <Grid container spacing={4} style={{padding: 30, marginTop: 100}}>
@@ -43,15 +44,6 @@ function LandingPage() {
                         Login
                     </Button>
                 </div>  
-                {/* <div>
-                    <img
-                        style={{
-                        width: "450px",
-                        height: "450px",
-                        objectFit: "contain"}}
-                        src="https://img.freepik.com/free-photo/science-dna-research-development-human_53876-121145.jpg?w=1060&t=st=1711182657~exp=1711183257~hmac=139d0cdd7e5a9f4dc39dfc8cd4798739731aadbf32f9ae882467841eda4809f8"
-                    />
-                </div>  */}
             </Grid>
             <Grid item xs={12} md={6} lg={6} style={{margin: 'auto'}} >
                 <img src={"https://img.freepik.com/free-photo/science-dna-research-development-human_53876-121145.jpg?w=1060&t=st=1711182657~exp=1711183257~hmac=139d0cdd7e5a9f4dc39dfc8cd4798739731aadbf32f9ae882467841eda4809f8"} width={"100%"} />
