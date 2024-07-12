900dfb9457c28b0737015b893c6dceb952739f14
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-12 17:39:33 +0530

    updated course card

--- src/CourseCard.jsx
+++ src/CourseCard.jsx
@@ -2,9 +2,10 @@ import { useParams } from "react-router-dom";
 // import Course from "./Course"
 import { useEffect, useState } from 'react';
 import AddCourse from "./AddCourse";
+import SingleCourseUpdate from "./SingleCourseUpdate";
 
 import axios from 'axios';
-import { Card, CardActions, CardContent, Typography, Button, TextField } from "@mui/material";
+import { Card, CardActions, CardContent, Typography, } from "@mui/material";
 
 const CourseCard = () => {
     let { courseId } = useParams();
@@ -38,7 +39,7 @@ const CourseCard = () => {
                 <CourseMini course={course} />
             </div>
             <div className="flex items-start pl-[4vw]">
-                <UpdateSingleCourse course={course} setCourse={setCourse} />
+                <SingleCourseUpdate course={course} setCourse={setCourse} />
             </div>
             
             
@@ -67,102 +68,6 @@ function CourseMini({course}) {
         </div>
     )
 }
-
-function UpdateSingleCourse({course, setCourse}) {
-
-    const [title, setTitle] = useState(course.title);
-    const [description, setDescription] = useState(course.description);
-    const [price, setPrice] = useState(course.price);
-    const [image, setImage] = useState(course.imageLink);
-
-    return (
-        <div>
-            
-            <Card 
-                style={{
-                    padding: '24px', 
-                    width: 600, 
-                    display:"flex", 
-                    flexDirection: "column", 
-                    justifyContent: "center", 
-                    alignItems: "center",
-                    borderRadius: '16px' 
-                }}
-            >
-            <h3 className="text-[1vw] font-semibold">Update Course details</h3>
-            <TextField 
-                value={title}
-                onChange={(e) => {setTitle(e.target.value)}}
-                style={{width: '520px'}} 
-                label="Title" 
-                variant="outlined" 
-                size="medium" 
-                margin="normal" 
-            />
-            <TextField  
-                onChange={(e) => {setDescription(e.target.value)}}
-               value={description}
-                style={{width: '520px'}} 
-                label="description" 
-                variant="outlined" 
-                size="medium" 
-                margin="normal" 
-            />
-            <TextField  
-                value={price}
-                onChange={(e) => {setPrice(e.target.value)}}
-                style={{width: '520px'}} 
-                label="price" 
-                variant="outlined" 
-                size="medium" 
-                margin="normal" 
-            />
-            <TextField 
-                onChange={(e) => {setImage(e.target.value)}}
-                value={image} 
-                style={{width: '520px'}} 
-                label="Image link" 
-                variant="outlined"
-                size="medium"
-                margin="normal"  
-            />           
-            <Button 
-                variant="contained" 
-                margin="normal"
-                size="large"
-                style={{
-                    marginTop: '16px',        
-                }}
-                onClick={async () => {
-                    axios.put('http://localhost:3000/admin/courses/' + course._id, {
-                        title: title,
-                        description: description,
-                        price: price,
-                        imageLink: image,
-                        published: true
-                    }, {
-                        headers: {
-                            "Content-Type": "application/json",
-                            "Authorization": `Bearer ${localStorage.getItem("token")}`
-                        }
-                    });
-                    let updatedCourse = {
-                        _id: course._id,
-                        title: title,
-                        description: description,
-                        imageLink: image,
-                        price
-                    };
-                    setCourse(updatedCourse)
-                }}
-            >
-                Update Course
-            </Button>
-        </Card>
-        </div>
-    )
-}
-
 
 
 export default CourseCard
