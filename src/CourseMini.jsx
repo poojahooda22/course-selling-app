a6903bb3f813ae3daa3c1fce940f771905c00723
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-12 17:53:45 +0530

    updated course mini

--- src/CourseMini.jsx
+++ src/CourseMini.jsx
@@ -3,15 +3,15 @@ import { Card, CardContent, Typography } from "@mui/material";
 function CourseMini({course}) {
     return (
         <div>
-            <Card sx={{ minWidth: 345 }}>
+            <Card sx={{ maxWidth: 520, borderRadius: '16px', }}>
                 <div className='w-[16vw] h-[6vw]'>
                     <img src={course.imageLink} className='w-full h-full object-cover'/>
                 </div>
                 <CardContent>
-                    <Typography gutterBottom variant="h5" color="text.secondary">
+                    <Typography variant="h5" color="text.primary" style={{fontSize: '1.2vw', fontWeight: 'bold'}} >
                     {course.title}
                     </Typography>
-                    <Typography variant="body">
+                    <Typography variant="body" style={{fontSize: '.8vw', fontWeight: 'normal'}} >
                     {course.description}
                     </Typography>
                     <p className="mt-2 text-[1vw] font-medium">{course.price}</p>
