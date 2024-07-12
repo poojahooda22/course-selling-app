a9040377a356a9497036d1631b9f0be7a16213c8
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-12 17:30:03 +0530

    updated course

---
+++ src/SingleCourseUpdate.jsx
@@ -0,0 +1,103 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { Card, Button, TextField } from "@mui/material";
+
+
+
+function SingleCourseUpdate({course, setCourse}) {
+
+    const [title, setTitle] = useState(course.title);
+    const [description, setDescription] = useState(course.description);
+    const [price, setPrice] = useState(course.price);
+    const [image, setImage] = useState(course.imageLink);
+
+    return (
+        <div>
+            
+            <Card 
+                style={{
+                    padding: '24px', 
+                    width: 600, 
+                    display:"flex", 
+                    flexDirection: "column", 
+                    justifyContent: "center", 
+                    alignItems: "center",
+                    borderRadius: '16px' 
+                }}
+            >
+            <h3 className="text-[1vw] font-semibold">Update Course details</h3>
+            <TextField 
+                value={title}
+                onChange={(e) => {setTitle(e.target.value)}}
+                style={{width: '520px'}} 
+                label="Title" 
+                variant="outlined" 
+                size="medium" 
+                margin="normal" 
+            />
+            <TextField  
+                onChange={(e) => {setDescription(e.target.value)}}
+               value={description}
+                style={{width: '520px'}} 
+                label="description" 
+                variant="outlined" 
+                size="medium" 
+                margin="normal" 
+            />
+            <TextField  
+                value={price}
+                onChange={(e) => {setPrice(e.target.value)}}
+                style={{width: '520px'}} 
+                label="price" 
+                variant="outlined" 
+                size="medium" 
+                margin="normal" 
+            />
+            <TextField 
+                onChange={(e) => {setImage(e.target.value)}}
+                value={image} 
+                style={{width: '520px'}} 
+                label="Image link" 
+                variant="outlined"
+                size="medium"
+                margin="normal"  
+            />           
+            <Button 
+                variant="contained" 
+                margin="normal"
+                size="large"
+                style={{
+                    marginTop: '16px',        
+                }}
+                onClick={async () => {
+                    axios.put('http://localhost:3000/admin/courses/' + course._id, {
+                        title: title,
+                        description: description,
+                        price: price,
+                        imageLink: image,
+                        published: true
+                    }, {
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Authorization": `Bearer ${localStorage.getItem("token")}`
+                        }
+                    });
+                    let updatedCourse = {
+                        _id: course._id,
+                        title: title,
+                        description: description,
+                        imageLink: image,
+                        price
+                    };
+                    setCourse(updatedCourse)
+                }}
+            >
+                Update Course
+            </Button>
+        </Card>
+        </div>
+    )
+}
+
+
+export default SingleCourseUpdate
