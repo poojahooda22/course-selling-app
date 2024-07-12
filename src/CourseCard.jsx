4aa1a5fff248ece0211be9a5a828f97c74ca1d94
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-12 17:41:37 +0530

    updated card

--- src/CourseCard.jsx
+++ src/CourseCard.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import SingleCourseUpdate from "./SingleCourseUpdate";
 import axios from 'axios';
 import { Card, CardContent, Typography, } from "@mui/material";
+import CourseMini from "./CourseMini";
 
 const CourseCard = () => {
     let { courseId } = useParams();
@@ -37,34 +38,11 @@ const CourseCard = () => {
             </div>
             <div className="flex items-start pl-[4vw]">
                 <SingleCourseUpdate course={course} setCourse={setCourse} />
-            </div>
-            
-            
+            </div>   
         </div>
     )
 }
 
-function CourseMini({course}) {
-    return (
-        <div>
-            <Card sx={{ minWidth: 345 }}>
-                <div className='w-[16vw] h-[6vw]'>
-                    <img src={course.imageLink} className='w-full h-full object-cover'/>
-                </div>
-                <CardContent>
-                    <Typography gutterBottom variant="h5" color="text.secondary">
-                    {course.title}
-                    </Typography>
-                    <Typography variant="body">
-                    {course.description}
-                    </Typography>
-                    <p className="mt-2 text-[1vw] font-medium">{course.price}</p>
-                </CardContent>
-                
-            </Card>  
-        </div>
-    )
-}
 
 
 export default CourseCard
