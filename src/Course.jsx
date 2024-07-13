50d2964c81c96b6002ca7a8e7c6d92cc3e53ad81
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-13 21:59:15 +0530

    added

--- src/Course.jsx
+++ src/Course.jsx
@@ -20,7 +20,7 @@ const Course = ({course}) => {
             <Typography gutterBottom variant="h5" color="text.primary" style={{fontWeight: 'bold'}}>
               {course.title}
             </Typography>
-              <Typography variant="body" style={{fontSize: '.8vw', fontWeight: 'medium',  display: 'block'}}>
+            <Typography variant="body" style={{fontSize: '.8vw', fontWeight: 'medium',  display: 'block'}}>
                   {course.description}
                 </Typography>
                 <Typography variant="paragraph" style={{fontSize: '1vw', fontWeight: 'bold', marginTop: '12px'}}>
