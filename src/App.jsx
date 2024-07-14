4919cc20b2ebd8eddb799cc4f3511f1517e9933d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-14 20:48:24 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
           <Routes>
             <Route path="/" element={<LandingPage userEmail={userEmail} />}></Route>
             <Route path="/signup" element={<Signup setUserEmail={setUserEmail}/>}  />
-          <Route path="/login" element={<Signin setUserEmail={setUserEmail}/>} />
+            <Route path="/login" element={<Signin setUserEmail={setUserEmail}/>} />
           <Route path="/addcourse" element={<AddCourse/>} />
           <Route path="/courses" element={<Courses/>} />
           <Route path="/course/:courseId" element={<CourseCard />} />
