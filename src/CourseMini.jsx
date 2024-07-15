7ec35d0f5c667836e1d3ce0fe6ca152d87f92bb8
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-15 14:24:22 +0530

    updated course mini

--- src/CourseMini.jsx
+++ src/CourseMini.jsx
@@ -1,6 +1,10 @@
 import { Card, CardContent, Typography } from "@mui/material";
+import { useRecoilValue } from "recoil";
 
-function CourseMini({course}) {
+function CourseMini(props) {
+    const title = useRecoilValue(courseTitle);
+    const imageLink = useRecoilValue(courseImage);
+    const description = useRecoilValue(courseDetails);
     return (
         <div>
             <Card sx={{ maxWidth: 520, borderRadius: '16px', }}>
@@ -9,12 +13,12 @@ function CourseMini({course}) {
                 </div>
                 <CardContent>
                     <Typography variant="h5" color="text.primary" style={{fontSize: '1.2vw', fontWeight: 'bold'}} >
-                    {course.title}
+                    {title}
                     </Typography>
                     <Typography variant="body" style={{fontSize: '.8vw', fontWeight: 'normal'}} >
-                    {course.description}
+                    {description}
                     </Typography>
-                    <p className="mt-2 text-[1vw] font-medium">Rs {course.price}</p>
+                    <p className="mt-2 text-[1vw] font-medium">Rs {price}</p>
                 </CardContent>
                 
             </Card>  
