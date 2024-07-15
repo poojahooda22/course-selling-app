7a4723da7ffb79e27e469e76f948986f3e579c92
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-15 13:09:20 +0530

    commit down

--- src/Courses.jsx
+++ src/Courses.jsx
@@ -15,21 +15,23 @@ const Courses = () => {
     })
   }
 
-  useEffect(() => {
-    function callback2(data){
-      setCourses(data.courses);
-    }
-    function callback1(res) {
-      res.json().then(callback2)
-    }
-    fetch('http://localhost:3000/admin/courses', {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${localStorage.getItem("token")}`
-      }
-    }).then(callback1)
-  })
+  //ugly way to write fetch request
+  
+  // useEffect(() => {
+  //   function callback2(data){
+  //     setCourses(data.courses);
+  //   }
+  //   function callback1(res) {
+  //     res.json().then(callback2)
+  //   }
+  //   fetch('http://localhost:3000/admin/courses', {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "Authorization": `Bearer ${localStorage.getItem("token")}`
+  //     }
+  //   }).then(callback1)
+  // })
 
   return (
     <div className='flex items-center justify-center gap-[2vw] flex-wrap px-[2vw] mt-[4vw]'>
