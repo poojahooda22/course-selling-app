4263060dea7856fd936340f4b5a3bf67fc3c8296
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-15 17:03:29 +0530

    updated

--- src/CourseMini.jsx
+++ src/CourseMini.jsx
@@ -16,8 +16,7 @@ function CourseMini(props) {
                     <Typography variant="h5" color="text.primary" style={{fontSize: '1.2vw', fontWeight: 'bold'}} >
                     {title}
                     </Typography>
-                    
-                    
+                        
                 </CardContent>
                 
             </Card>  
