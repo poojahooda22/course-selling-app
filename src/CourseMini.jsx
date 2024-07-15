f4aa83ef80378cfd99865085c2962672cbd24955
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-15 22:13:58 +0530

    added

--- src/CourseMini.jsx
+++ src/CourseMini.jsx
@@ -1,6 +1,6 @@
 import { Card, CardContent, Typography } from "@mui/material";
 import { useRecoilValue } from "recoil";
-import { course, courseImage, courseTitle } from "./store/selectors/course";
+import { cours, courseImage, courseTitle } from "./store/selectors/course";
 
 function CourseMini(props) {
     const title = useRecoilValue(courseTitle);
