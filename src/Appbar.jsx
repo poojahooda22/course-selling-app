78b9e35443449bbcf3199d29761f29c5fc440ee8
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-25 21:54:09 +0530

    updated appbar

--- src/Appbar.jsx
+++ src/Appbar.jsx
@@ -19,7 +19,7 @@ const Appbar = () => {
     if (userEmail) {
         return <>
         <div className='w-full flex items-center 
-        justify-between px-[2vw]'>
+            justify-between px-[2vw]'>
             <h2 className='text-[1.5vw] font-bold'>Coursera</h2> 
             <div className='flex gap-4'>
                 <Button 
