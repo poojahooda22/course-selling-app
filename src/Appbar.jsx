9825327bc4c96ebca065dcf0f95c1aeac0531992
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-25 21:58:17 +0530

    added

--- src/Appbar.jsx
+++ src/Appbar.jsx
@@ -70,7 +70,8 @@ const Appbar = () => {
     <>
         <div 
             className='w-full flex items-center 
-            justify-between px-[2vw]'>
+            justify-between px-[2vw]'
+            >
             <h2 className='text-[1.5vw] font-bold'>
                 Courera
             </h2>
