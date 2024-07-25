eae81ccabb3d67b7c0439e8135848d0adc147d9e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-25 21:57:24 +0530

    added

--- src/Appbar.jsx
+++ src/Appbar.jsx
@@ -69,7 +69,8 @@ const Appbar = () => {
     return (
     <>
         <div className='w-full flex items-center justify-between px-[2vw]'>
-            <h2 className='text-[1.5vw] font-bold'>Courera</h2>
+            <h2 className='text-[1.5vw] font-bold'>
+                Courera</h2>
             <div className='flex gap-4'>
                 <Button 
                     variant="contained" 
