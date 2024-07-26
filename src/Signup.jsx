461fe1eb2af8c4583ac18ea6a02714f92177c199
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-26 21:20:38 +0530

    removed the function

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -83,7 +83,7 @@ function Signup() {
                         // }
                         // fetch('http://localhost:3000/admin/signup', {
                         //     method: "POST",
-                        //     body: JSON.stringify({username: email,
+                        //     body: JSON.stringify({username: 
 
                         // this is another way to write the fetch function with axios external library
 
