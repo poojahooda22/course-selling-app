f7c59c55dee9723ac02719abc6e5e6198e5b8671
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-26 22:17:00 +0530

    added signup

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -79,7 +79,7 @@ function Signup() {
                         //    window.location = "/"
                         // }
                         // function callback(response) {
-                        //   response.json().then
+                        //   response.jso
 
                         // this is another way to write the fetch function with axios external library
 
