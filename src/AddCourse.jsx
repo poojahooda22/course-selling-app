37c2fcb5a9d6168d0da021068ee863a785c3231b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-30 21:50:25 +0530

    added

--- src/AddCourse.jsx
+++ src/AddCourse.jsx
@@ -81,7 +81,7 @@ function AddCourse() {
                     //         published: 
                     //     }),
                     //     headers: {
-                    //         "Content-Type": "application/json",    
+                    //         "Content-Type": "applicati
 
                     await axios.post('http://localhost:3000/admin/courses/', {
                         title: title,
