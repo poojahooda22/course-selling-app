813bda9029f14b2b3bf7185f90e39d04040ebf82
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-06 22:26:33 +0530

    added

--- src/SingleCourseUpdate.jsx
+++ src/SingleCourseUpdate.jsx
@@ -94,7 +94,7 @@ function SingleCourseUpdate() {
                 }}
             >
                 Update Course
-            </Button>
+                </Button>
             </Card>
         </div>
     )
