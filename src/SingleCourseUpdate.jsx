2fc3b382fe7c6a0153be2a276a937995295cbf3d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-06 23:05:49 +0530

    added

--- src/SingleCourseUpdate.jsx
+++ src/SingleCourseUpdate.jsx
@@ -71,7 +71,7 @@ function SingleCourseUpdate() {
                         marginTop: '16px',        
                     }}
                     onClick={async () => {
-                    axios.put('http://localhost:3000/admin/courses/' +  courseDetails.course._id, {
+                        axios.put('http://localhost:3000/admin/courses/' +  courseDetails.course._id, {
                         title: title,
                         description: description,
                         price: price,
